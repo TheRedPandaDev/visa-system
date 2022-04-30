0153f502947486cc0f0cdc9f9b0db52cee901b4c
Dmitry Milya <dmilya2000@gmail.com>
2022-04-30 13:22:53 +0300

    Added incoming-application-service

---
+++ incoming-application-service/src/main/java/com/dmitrymilya/visa/incomingapplicationservice/IncomingApplicationServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dmitrymilya.visa.incomingapplicationservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class IncomingApplicationServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(IncomingApplicationServiceApplication.class, args);
+	}
+
+}
---
+++ incoming-application-service/src/test/java/com/dmitrymilya/visa/incomingapplicationservice/IncomingApplicationServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmitrymilya.visa.incomingapplicationservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class IncomingApplicationServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
