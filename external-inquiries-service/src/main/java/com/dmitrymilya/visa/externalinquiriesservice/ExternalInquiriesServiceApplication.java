4ac73c6186c0f23635f238c2ae405abef35f87fb
Dmitry Milya <dmilya2000@gmail.com>
2022-05-03 13:51:42 +0300

    Added External Inquiries Service

---
+++ external-inquiries-service/src/main/java/com/dmitrymilya/visa/externalinquiriesservice/ExternalInquiriesServiceApplication.java
@@ -0,0 +1,13 @@
+package com.dmitrymilya.visa.externalinquiriesservice;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ExternalInquiriesServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ExternalInquiriesServiceApplication.class, args);
+	}
+
+}
---
+++ external-inquiries-service/src/test/java/com/dmitrymilya/visa/externalinquiriesservice/ExternalInquiriesServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.dmitrymilya.visa.externalinquiriesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ExternalInquiriesServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
